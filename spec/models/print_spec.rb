b97211af9f4841b42070e679fac5d03d5d4f9786
Anthony Good <anthony@quipper.com>
2014-11-09 11:00:51 +0000

    update thumbnails

--- spec/models/print_spec.rb
+++ spec/models/print_spec.rb
@@ -1,31 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Print, :type => :model do
-  let(:subject) { Print.create }
-
-  it "can be initialized with a filename" do
-    new_print = Print.create_with_image('filename')
-
-    expect(new_print.large_url).to eq('/filename.jpg')
-  end
-
-  it "won't duplicate the file extension if you include it by mistake!" do
-    new_print = Print.create_with_image('filename.jpg')
-
-    expect(new_print.large_url).to eq('/filename.jpg')
-  end
-
-  it "can accept a file extension other than jpg" do
-    new_print = Print.create_with_image('filename', 'png')
-
-    expect(new_print.large_url).to eq('/filename.png')
-  end
-
-  it "will accept a whole filename" do
-    new_print = Print.create_with_image('filename.png')
-
-    expect(new_print.large_url).to eq('/filename.png')
-  end
+  let(:etching) { Etching.create title: "Boo Radley"}
 
   context "invalid" do
     it "without an etching" do
@@ -33,12 +9,41 @@ RSpec.describe Print, :type => :model do
     end
   end
 
-  context "valid" do
-    let(:etching) { Etching.create(title: "The Golden Goose") }
-    
-    it "with an etching" do
-      expect(Print.new(etching: etching)).to be_valid
+  describe "create_with_image" do
+    let(:print) { Print.create_with_image etching }
+
+    it "creates a new print" do
+      expect{ print }.to change { Print.count }.by 1
     end
+
+    it "associates the new print with the etching" do
+      print = Print.create_with_image etching
+
+      expect(etching.prints.count).to eq 1
+      expect(etching.prints.last).to eq print
+      expect(print.etching).to eq etching
+    end
+
+    it "can accept optional file extension" do
+      new_print = Print.create_with_image(etching, 'png')
+
+      expect(new_print.large_url).to eq('/boo_radley.png')
+    end
+
+    it "won't duplicate the file extension if you include it by mistake!" do
+      new_print = Print.create_with_image(etching, 'jpg')
+
+      expect(new_print.large_url).to eq('/boo_radley.jpg')
+    end
+
+    context "valid" do
+      let(:etching) { Etching.create(title: "The Golden Goose") }
+      
+      it "with an etching" do
+        expect(Print.new(etching: etching)).to be_valid
+      end
+    end
+
   end
 
 end
