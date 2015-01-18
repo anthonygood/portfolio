acfdd02a886bfbabf0e0fe984e3c4d114133687c
Anthony Good <anthony@quipper.com>
2015-01-18 16:39:27 +0000

    WIP -- exhibit

--- app/controllers/etchings_controller.rb
+++ app/controllers/etchings_controller.rb
@@ -1,11 +1,32 @@
 class EtchingsController < ApplicationController
+
+  ORDER = [
+    15, 10, 5
+  ]
+
   def index
     @etchings = Etching.includes(:prints).all
   end
 
   def show
     @etching = Etching.includes(:prints).find params[:id]
+    @next    = Etching.next @etching
+    @prev    = Etching.previous @etching
     
     redirect_to :etchings unless @etching.listed
   end
+
+  def exhibit
+    @etchings = Etching.all
+  end
+
+  private
+
+  def arbitrary_order(ids)
+    Etching.find(ids)
+           .index_by(&:id)
+           .slice(*ids)
+           .values
+  end
+
 end
