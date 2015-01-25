f12775f32e91bc73de7cdc1d3b1c7002d4d9b7cf
Anthony Good <anthony@quipper.com>
2015-01-25 19:08:07 +0000

    WIP -- #colours_mentioned

--- app/helpers/etchings_helper.rb
+++ app/helpers/etchings_helper.rb
@@ -1,2 +1,9 @@
 module EtchingsHelper
+  def colours_mentioned(string)
+    colours = [/red/i, /blue/i, /yellow/i]
+
+    colours.inject({}) do |hash, colour|
+      hash.tap {|hash| hash[colour.source.to_sym] = string.scan(colour).count }
+    end.reject { |k,v| v < 1 }
+  end
 end
